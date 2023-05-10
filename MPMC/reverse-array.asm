231f7f461c51cb750dbffd1b2ac722c7fc5731e6
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-05-10 12:55:32 +0530

    Update reverse-array.asm

---
+++ MPMC/reverse-array.asm
@@ -0,0 +1,19 @@
+ORG 100H;
+.DATA ARRAY1 DB 01H, 02H, 03H, 04H, 05H, 06H, 07H, 08H, 09H, 0AH, 0BH, 0CH, 0DH, 0EH, 0FH;
+.CODE MOV SI, OFFSET ARRAY1;
+MOV CL, 0FH;
+MOV CH, 00H;
+MOV DI, SI;
+ADD DI, 0EH;
+L1: MOV AL, [SI];
+    MOV BL, [DI];
+    XCHG [SI], BL;
+    XCHG [DI], AL;
+    INC SI;
+    DEC DI;
+    INC CH;
+    DEC CL;
+    CMP CL, CH;
+    JG L1;
+    .EXIT
+    END
