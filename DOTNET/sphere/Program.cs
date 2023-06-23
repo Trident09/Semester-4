1f668cb85bf08cb545eede5c193d647d240c0737
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-06-23 07:57:34 +0000

    sphere

---
+++ DOTNET/sphere/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace sphere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sphere();
+        }
+
+
+        static void sphere()
+        {
+            Console.WriteLine("Enter the radius of the sphere");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double volume = 4.00 / 3.00 * Math.PI * radius * radius * radius;
+            Console.WriteLine("The volume of the sphere is " + volume);
+            double surfaceArea = 4 * Math.PI * radius * radius;
+            Console.WriteLine("The surface area of the sphere is " + surfaceArea);
+        }
+    }
+}
