fab0e3464fb461c7865c6b333944ab3f97b7437b
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-06-23 07:29:28 +0000

    check Prime

---
+++ DOTNET/Prime-Check/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CheckPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            game();
+        }
+
+
+        static void game()
+        {
+            Console.WriteLine("Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("Not a prime number");
+                    break;
+                }
+                else if (i == num - 1)
+                {
+                    Console.WriteLine("Prime number");
+                    break;
+                }
+            }
+        }
+    }
+}
