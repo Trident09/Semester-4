d3a0e48387df6839ee093eb8642ea00fba89a3aa
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-06-23 07:38:55 +0000

    swap

---
+++ DOTNET/swap2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace swapwithout3rdvariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            swap();
+        }
+
+
+        static void swap()
+        {
+            Console.WriteLine("Enter the number 1");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the number 2");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" x is " + x);
+            Console.WriteLine(" y is " + y);
+            x = x + y;
+            y = x - y;
+            x = x - y;
+            Console.WriteLine("After Swap x is " + x);
+            Console.WriteLine("After Swap y is " + y);
+        }
+    }
+}
