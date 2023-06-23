6371b06e3ba27587db42ad45fc621c4144c82052
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-06-23 07:43:39 +0000

    floyd

---
+++ DOTNET/floyd/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace floyd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            floyd();
+        }
+
+
+        static void floyd()
+        {
+            Console.WriteLine("Enter the number of rows");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            int num = 1;
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    Console.Write(num + " ");
+                    num++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
