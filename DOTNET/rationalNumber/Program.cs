cf2b193e44e0cec34034b9ec370e037a570c0233
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-06-23 07:33:21 +0000

    rational number

---
+++ DOTNET/rationalNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace RationalNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational();
+        }
+
+
+        static void Rational()
+        {
+            Console.WriteLine("Enter the number");
+            double num = Convert.ToDouble(Console.ReadLine());
+            if (num % 1 == 0)
+            {
+                Console.WriteLine("The number is rational");
+            }
+            else
+            {
+                Console.WriteLine("The number is irrational");
+            }
+        }
+    }
+}
