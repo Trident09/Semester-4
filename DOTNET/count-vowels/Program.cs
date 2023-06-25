1d90d28014aec3ae3d397f0863f7f3b8d3bc1394
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-06-25 19:32:38 +0000

    count vowels in a sentence

---
+++ DOTNET/count-vowels/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace vowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            count();
+        }
+
+
+        static void count()
+        {
+            int total = 0;
+            // Build a list of vowels up front:
+            var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+            Console.WriteLine("Enter a Sentence");
+            string sentence = Console.ReadLine().ToLower();
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (vowels.Contains(sentence[i]))
+                {
+                    total++;
+                }
+            }
+            Console.WriteLine("Your total number of vowels is: {0}", total);
+        }
+    }
+}
