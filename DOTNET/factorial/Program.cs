4b0083e558fd7318895b23bd88a18f3c1aa3ec41
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-06-25 20:15:25 +0000

    factorial

---
+++ DOTNET/factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            factorial();
+        }
+
+
+        static void factorial()
+        {
+            int num, n, fac;
+            Console.WriteLine("Please enter a number to get its factorial: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            num = 1;
+            fac = 1;
+            while (num < n)
+            {
+                fac = fac * (num+1);
+                num++;
+            }
+            Console.WriteLine("Factorial of {0}! = {1}\n", num, fac);
+        }
+    }
+}
