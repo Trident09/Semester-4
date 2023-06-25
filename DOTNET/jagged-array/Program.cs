4a88e5e53d529477766039b44a8be1fd1d8f0608
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-06-25 06:28:42 +0000

    jagged array

---
+++ DOTNET/jagged-array/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace sphere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            jagged();
+        }
+
+
+        static void jagged()
+        {
+            Console.WriteLine("Enter the number of rows");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            int[][] arr = new int[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                Console.WriteLine("Enter the numbers in the " + (i + 1) + "th row ");
+                int size = Convert.ToInt32(Console.ReadLine());
+                arr[i] = new int[size];
+                for (int j = 0; j < size; j++)
+                {
+                    Console.WriteLine("Enter the element " + (j + 1));
+                    arr[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            //print the jagged 2d array
+            Console.WriteLine("The jagged 2D array is ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.Write(arr[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
