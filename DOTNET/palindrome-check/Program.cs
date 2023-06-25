16d115e9b5c31b5fe39dadca9b93f8cf46f11774
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-06-25 17:01:09 +0000

    palindrome-check

---
+++ DOTNET/palindrome-check/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string : ");
+            string value = Console.ReadLine();
+             if (IsPalindrome(value))
+            {
+                Console.WriteLine("Palindrome");
+            } else {
+                Console.WriteLine("Not Palindrome");
+            }
+
+        }
+
+
+        public static bool IsPalindrome(string value)
+        {
+            int i = 0;
+            int j = value.Length - 1;
+            while (true)
+            {
+                if (i > j)
+                {
+                    return true;
+                }
+                char a = value[i];
+                char b = value[j];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+        }
+    }
+}
