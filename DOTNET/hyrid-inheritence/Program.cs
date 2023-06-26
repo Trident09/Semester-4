5c2443e8670295152968625f9bc4ce4b3f090fab
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-06-26 06:28:27 +0000

    hybrid inheritence

---
+++ DOTNET/hyrid-inheritence/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+interface IAnimal
+{
+    void Eat();
+}
+
+class Animal
+{
+    public void Sleep()
+    {
+        Console.WriteLine("Animal is sleeping.");
+    }
+}
+
+class Cat : Animal, IAnimal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Cat is eating.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cat cat = new Cat();
+        cat.Eat();    // Inherited from IAnimal
+        cat.Sleep();  // Inherited from Animal
+    }
+}
