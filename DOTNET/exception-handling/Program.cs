7bf91f67962d772402552315e71b60aedbe88d08
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-06-26 06:31:48 +0000

    exception handling

---
+++ DOTNET/exception-handling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int numerator = 10;
+            int denominator = 0;
+            int result = numerator / denominator;
+            Console.WriteLine("Result: " + result); // This line won't execute
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Finally block executed.");
+        }
+
+        Console.WriteLine("Program continues after exception handling.");
+    }
+}
