df9ae25029830d117ae19d8b2375c6c0048216af
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-06-26 06:37:45 +0000

    nested catch block

---
+++ DOTNET/nested-catch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int[] numbers = { 1, 2, 3 };
+            int index = 4;
+            int result = numbers[index]; // This line will throw an IndexOutOfRangeException
+            Console.WriteLine("Result: " + result); // This line won't execute
+        }
+        catch (Exception ex)
+        {
+            try
+            {
+                throw new InvalidOperationException("Nested exception occurred.", ex);
+            }
+            catch (InvalidOperationException nestedEx)
+            {
+                Console.WriteLine("Nested exception: " + nestedEx.Message);
+                Console.WriteLine("Inner exception: " + nestedEx.InnerException.Message);
+            }
+        }
+
+        Console.WriteLine("Program continues after exception handling.");
+    }
+}
