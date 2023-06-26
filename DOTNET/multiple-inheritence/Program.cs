ed64f120c67d786751a00d835be8ecaf3d504846
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-06-26 06:25:36 +0000

    multiple inheritence

---
+++ DOTNET/multiple-inheritence/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+interface IAnimal
+{
+    void Eat();
+}
+
+interface IFlyable
+{
+    void Fly();
+}
+
+class Bird : IAnimal, IFlyable
+{
+    public void Eat()
+    {
+        Console.WriteLine("Bird is eating.");
+    }
+
+    public void Fly()
+    {
+        Console.WriteLine("Bird is flying.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bird bird = new Bird();
+        bird.Eat();
+        bird.Fly();
+    }
+}
