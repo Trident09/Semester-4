8b68af513cd2d1282d688ffa5862f8249940a615
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-06-26 06:26:56 +0000

    multilevel inheritence

---
+++ DOTNET/multilevel-inheritence/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Animal is eating.");
+    }
+}
+
+class Mammal : Animal
+{
+    public void Run()
+    {
+        Console.WriteLine("Mammal is running.");
+    }
+}
+
+class Dog : Mammal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Dog is barking.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        dog.Eat();   // Inherited from Animal
+        dog.Run();   // Inherited from Mammal
+        dog.Bark();  // Dog-specific behavior
+    }
+}
