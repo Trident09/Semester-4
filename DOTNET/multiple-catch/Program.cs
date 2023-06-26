032ef6160a481eef71f3745f656a69728ba21354
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-06-26 06:36:20 +0000

    multiple catch block

---
+++ DOTNET/multiple-catch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int[] numbers = { 1, 2, 3 };
+            int index = 4;
+            int result = numbers[index]; // This line will throw an IndexOutOfRangeException
+            Console.WriteLine("Result: " + result); // This line won't execute
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("Index out of range error: " + ex.Message);
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Divide by zero error: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Generic error: " + ex.Message);
+        }
+
+        Console.WriteLine("Program continues after exception handling.");
+    }
+}
