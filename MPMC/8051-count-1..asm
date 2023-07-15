32b9c06f338d3847c77cbbcf2f88788743147dbe
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-07-16 00:21:04 +0530

    count the number of 1’s

--- MPMC/8051-count-1..asm
+++ MPMC/8051-count-1..asm
@@ -1,1 +1,23 @@
+ORG 0x0000
 
+MAIN:
+    MOV A, #0
+    MOV R0, #8
+    
+LOOP:
+    MOV C, A
+    RRC A
+    
+    JC INCREMENT
+    
+DECREMENT:
+    DJNZ R0, LOOP
+    SJMP EXIT
+    
+INCREMENT:
+    INC A
+    JMP DECREMENT
+    
+EXIT:
+    
+END
